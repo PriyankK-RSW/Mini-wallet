d725fad7a8afdc9ccea8429401ad3e375839084a
Priyank K <priyankk@rainstreamweb.com>
2026-01-22 13:08:33 +0530

    [fix] : Deployment conifg fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    host: true,         
+    host: true,
     strictPort: true,
     allowedHosts: 'all',
   },
+
+  build: {
+    chunkSizeWarningLimit: 1000
+  }
 })
